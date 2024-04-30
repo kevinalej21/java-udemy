02e364fcbc89d6b2113270fb7ff47c7543996c4b
kevinalej21 <147507805+kevinalej21@users.noreply.github.com>
2024-04-30 16:52:16 -0500

    Add files via upload

---
+++ Java_udemy/PruebaPersona.java
@@ -0,0 +1,17 @@
+public class PruebaPersona {
+
+    public static void main(String[] args) {
+
+        persona Persona1 = new persona();
+        Persona1.Nombre = "Juan";
+        Persona1.Apellido = "perez";
+        Persona1.desplegarInformacion();
+
+        
+        persona Persona2 = new persona();
+
+        System.out.println("Persona2"+Persona2);
+
+    }
+
+}
---
+++ Java_udemy/persona.java
@@ -0,0 +1,14 @@
+public class persona {
+    
+    String Nombre ;
+    String Apellido;
+
+    public void desplegarInformacion(){
+        
+        System.out.println("Nombre :"+Nombre);
+        System.out.println("Apellido : "+Apellido);
+    }
+
+    
+
+}
